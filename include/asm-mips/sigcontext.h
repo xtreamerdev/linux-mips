f4c5bd3491350f88a57ffe83aa8cf4eadd26e6d5
Ralf Baechle <ralf@linux-mips.org>
1999-08-25 23:52:31 +0000

    - pause(2) is gone.  - fix type in init_task.c  - cleanup errno.h  - rewrite signal handling.  2.2 was the testbed; I'll tomorrow create 2.3    and 64-bit versions.

--- include/asm-mips/sigcontext.h
+++ include/asm-mips/sigcontext.h
@@ -1,38 +1,32 @@
-/*
- * include/asm-mips/sigcontext.h
+/* $Id$
  *
  * This file is subject to the terms and conditions of the GNU General Public
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
  *
- * Copyright (C) 1996, 1997 by Ralf Baechle
- *
- * $Id: sigcontext.h,v 1.4 1997/12/01 18:00:42 ralf Exp $
+ * Copyright (C) 1996, 1997, 1999 by Ralf Baechle
+ * Copyright (C) 1999 Silicon Graphics, Inc.
  */
-#ifndef __ASM_MIPS_SIGCONTEXT_H
-#define __ASM_MIPS_SIGCONTEXT_H
+#ifndef _ASM_SIGCONTEXT_H
+#define _ASM_SIGCONTEXT_H
 
 /*
  * Keep this struct definition in sync with the sigcontext fragment
  * in arch/mips/tools/offset.c
  */
 struct sigcontext {
-	unsigned int       sc_regmask;		/* Unused */
-	unsigned int       sc_status;
-	unsigned long long sc_pc;
 	unsigned long long sc_regs[32];
-	unsigned long long sc_fpregs[32];	/* Unused */
-	unsigned int       sc_ownedfp;
-	unsigned int       sc_fpc_csr;		/* Unused */
-	unsigned int       sc_fpc_eir;		/* Unused */
-	unsigned int       sc_ssflags;		/* Unused */
+	unsigned long long sc_fpregs[32];
 	unsigned long long sc_mdhi;
 	unsigned long long sc_mdlo;
+	unsigned long long sc_pc;
+	unsigned int       sc_status;
+	unsigned int       sc_ownedfp;
+	unsigned int       sc_fpc_csr;
+	unsigned int       sc_fpc_eir;
 
-	unsigned int       sc_cause;		/* Unused */
-	unsigned int       sc_badvaddr;		/* Unused */
-
-	unsigned long      sc_sigset[4];	/* kernel's sigset_t */
+	unsigned int       sc_cause;
+	unsigned int       sc_badvaddr;
 };
 
-#endif /* __ASM_MIPS_SIGCONTEXT_H */
+#endif /* _ASM_SIGCONTEXT_H */
