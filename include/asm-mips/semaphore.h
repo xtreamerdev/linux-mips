2bca60cc5f7cfbd85d95fd422650b9ddd791b11a
Ralf Baechle <ralf@linux-mips.org>
1999-10-20 21:22:43 +0000

    And the same for 2.2.  Sorry guys, I haven't even tried to compile this; the old code was completly broken.

--- include/asm-mips/semaphore.h
+++ include/asm-mips/semaphore.h
@@ -1,4 +1,4 @@
-/* $Id: semaphore.h,v 1.5.2.1 1999/06/14 21:43:01 ralf Exp $
+/* $Id: semaphore.h,v 1.10 1999/10/09 00:01:43 ralf Exp $
  *
  * SMP- and interrupt-safe semaphores..
  *
@@ -8,29 +8,32 @@
  *
  * (C) Copyright 1996  Linus Torvalds
  * (C) Copyright 1998, 1999  Ralf Baechle
+ * (C) Copyright 1999  Silicon Graphics, Inc.
  */
-#ifndef __ASM_MIPS_SEMAPHORE_H
-#define __ASM_MIPS_SEMAPHORE_H
+#ifndef _ASM_SEMAPHORE_H
+#define _ASM_SEMAPHORE_H
 
 #include <asm/system.h>
 #include <asm/atomic.h>
-#include <asm/spinlock.h>
+#include <linux/spinlock.h>
+#include <linux/config.h>
 
 struct semaphore {
 	atomic_t count;
 	atomic_t waking;
-	struct wait_queue * wait;
+	struct wait_queue *wait;
 };
 
-#define MUTEX ((struct semaphore) { ATOMIC_INIT(1), ATOMIC_INIT(0), NULL })
-#define MUTEX_LOCKED ((struct semaphore) { ATOMIC_INIT(0), ATOMIC_INIT(0), NULL })
+#define MUTEX ((struct semaphore) \
+ { ATOMIC_INIT(1), ATOMIC_INIT(0), NULL })
+#define MUTEX_LOCKED ((struct semaphore) \
+ { ATOMIC_INIT(0), ATOMIC_INIT(0), NULL })
+
 
 asmlinkage void __down(struct semaphore * sem);
 asmlinkage int  __down_interruptible(struct semaphore * sem);
 asmlinkage int __down_trylock(struct semaphore * sem);
 asmlinkage void __up(struct semaphore * sem);
-
-extern spinlock_t semaphore_wake_lock;
 
 #define sema_init(sem, val)	atomic_set(&((sem)->count), val)
 
@@ -43,16 +46,45 @@ extern inline void down(struct semaphore * sem)
 extern inline int down_interruptible(struct semaphore * sem)
 {
 	int ret = 0;
+
 	if (atomic_dec_return(&sem->count) < 0)
 		ret = __down_interruptible(sem);
 	return ret;
 }
 
+#if !defined(CONFIG_CPU_HAS_LLSC)
+
+extern inline int down_trylock(struct semaphore * sem)
+{
+	int ret = 0;
+	if (atomic_dec_return(&sem->count) < 0)
+		ret = __down_trylock(sem);
+	return ret;
+}
+
+#else
+
 /*
  * down_trylock returns 0 on success, 1 if we failed to get the lock.
  *
  * We must manipulate count and waking simultaneously and atomically.
- * Do this by using ll/sc on the pair of 32-bit words.
+ * Here, we this by using ll/sc on the pair of 32-bit words.  This
+ * won't work on MIPS32 platforms, however, and must be rewritten.
+ *
+ * Pseudocode:
+ *
+ *   Decrement(sem->count)
+ *   If(sem->count >=0) {
+ *	Return(SUCCESS)			// resource is free
+ *   } else {
+ *	If(sem->waking <= 0) {		// if no wakeup pending
+ *	   Increment(sem->count)	// undo decrement
+ *	   Return(FAILURE)
+ *      } else {
+ *	   Decrement(sem->waking)	// otherwise "steal" wakeup
+ *	   Return(SUCCESS)
+ *	}
+ *   }
  */
 extern inline int down_trylock(struct semaphore * sem)
 {
@@ -61,36 +93,67 @@ extern inline int down_trylock(struct semaphore * sem)
 #ifdef __MIPSEB__
 	__asm__ __volatile__("
 			.set	mips3
+
 		0:	lld	%1, %4
 			dli	%3, 0x0000000100000000
-			sltu	%0, %1, $0
-
-			bltz	%1, 1f
-			move	%3, $0
+			dsubu	%1, %3
+			li	%0, 0
+			bgez	%1, 2f
+			sll	%2, %1, 0
+			blez	%2, 1f
+			daddiu	%1, %1, -1
+			b	2f
 		1:
-
-			sltu	%2, %1, $0
-			and	%0, %0, %2
-			bnez	%0, 2f
-
-			subu	%0, %3
-			scd	%1, %4
-
-			beqz	%1, 0b
+			daddu	%1, %1, %3
+			li	%0, 1
 		2:
+			scd	%1, %4
+			beqz	%1, 0b
 
 			.set	mips0"
 		: "=&r"(ret), "=&r"(tmp), "=&r"(tmp2), "=&r"(sub)
 		: "m"(*sem)
 		: "memory");
-#endif
 
-#ifdef __MIPSEL__
-#error "FIXME: down_trylock doesn't support little endian machines yet."
+#elif defined(__MIPSEL__)
+
+	__asm__ __volatile__("
+			.set	mips3
+
+		0:	lld	%1, %4
+			li	%0, 0
+			sll	%2, %1, 0
+			addiu	%2, %2, -1
+			bgez	%2, 2f
+			bltz	%1, 1f
+			dsll32	%2, %2, 0
+			dsrl32  %2, %2, 0
+			dli	%3, 0x0000000100000000
+			dsubu	%1, %3
+			b	2f
+		1:
+			li	%0, 1
+			b	3f
+		2:
+			dsrl32	%1, %1, 0
+			dsll32	%1, %1, 0
+			or	%1, %1, %2
+		3:
+			scd	%1, %4
+			beqz	%1, 0b
+
+			.set	mips0"
+		: "=&r"(ret), "=&r"(tmp), "=&r"(tmp2), "=&r"(sub)
+		: "m"(*sem)
+		: "memory");
+#else
+#error "MIPS but neither __MIPSEL__ nor __MIPSEB__"
 #endif
 
 	return ret;
 }
+
+#endif
 
 /*
  * Note! This is subtle. We jump to wake people up only if
@@ -102,4 +165,4 @@ extern inline void up(struct semaphore * sem)
 		__up(sem);
 }
 
-#endif /* __ASM_MIPS_SEMAPHORE_H */
+#endif /* _ASM_SEMAPHORE_H */
