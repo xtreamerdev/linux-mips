7846fda1bb422d75d1aee02a6726927673cb5c69
Ralf Baechle <ralf@linux-mips.org>
2000-02-19 00:31:56 +0000

    Fix copy_from_user() in modules.

--- include/asm-mips/uaccess.h
+++ include/asm-mips/uaccess.h
@@ -1,4 +1,4 @@
-/* * $Id: uaccess.h,v 1.8 1999/01/04 16:09:27 ralf Exp $
+/* * $Id: uaccess.h,v 1.8.2.1 1999/10/22 00:00:55 ralf Exp $
  *
  * This file is subject to the terms and conditions of the GNU General Public
  * License.  See the file "COPYING" in the main directory of this archive
@@ -322,10 +322,12 @@ extern size_t __copy_user(void *__to, const void *__from, size_t __n);
 		"move\t$4, %1\n\t" \
 		"move\t$5, %2\n\t" \
 		"move\t$6, %3\n\t" \
+		".set\tnoreorder\n\t" \
 		".set\tnoat\n\t" \
+		__MODULE_JAL(__copy_user) \
 		"addu\t$1, %2, %3\n\t" \
 		".set\tat\n\t" \
-		__MODULE_JAL(__copy_user) \
+		".set\treorder\n\t" \
 		"move\t%0, $6" \
 		: "=r" (__cu_len) \
 		: "r" (__cu_to), "r" (__cu_from), "r" (__cu_len) \
@@ -369,10 +371,12 @@ extern size_t __copy_user(void *__to, const void *__from, size_t __n);
 			"move\t$4, %1\n\t" \
 			"move\t$5, %2\n\t" \
 			"move\t$6, %3\n\t" \
+			".set\tnoreorder\n\t" \
 			".set\tnoat\n\t" \
+			__MODULE_JAL(__copy_user) \
 			"addu\t$1, %2, %3\n\t" \
 			".set\tat\n\t" \
-			__MODULE_JAL(__copy_user) \
+			".set\treorder\n\t" \
 			"move\t%0, $6" \
 			: "=r" (__cu_len) \
 			: "r" (__cu_to), "r" (__cu_from), "r" (__cu_len) \
