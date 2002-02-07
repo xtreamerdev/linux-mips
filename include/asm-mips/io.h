8798ff6144b342ce42efa677403f89c9a17c53fd
Ralf Baechle <ralf@linux-mips.org>
2002-02-07 00:38:59 +0000

    Make ioremap accept a 64-bit physical address as argument.

--- include/asm-mips/io.h
+++ include/asm-mips/io.h
@@ -140,12 +140,12 @@ typedef unsigned long phys_t;
 
 extern void * __ioremap(phys_t offset, phys_t size, unsigned long flags);
 
-static inline void *ioremap(unsigned long offset, unsigned long size)
+static inline void *ioremap(phys_t offset, unsigned long size)
 {
 	return __ioremap(offset, size, _CACHE_UNCACHED);
 }
 
-static inline void *ioremap_nocache(unsigned long offset, unsigned long size)
+static inline void *ioremap_nocache(phys_t offset, unsigned long size)
 {
 	return __ioremap(offset, size, _CACHE_UNCACHED);
 }
