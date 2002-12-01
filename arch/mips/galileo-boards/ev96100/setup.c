3c62980db50647c1839d8af08660b922aca9cef4
Ralf Baechle <ralf@linux-mips.org>
2002-12-01 20:59:10 +0000

    Define bus_error_init.

--- arch/mips/galileo-boards/ev96100/setup.c
+++ arch/mips/galileo-boards/ev96100/setup.c
@@ -65,17 +65,20 @@ extern struct resource ioport_resource;
 
 unsigned char mac_0_1[12];
 
+void __init bus_error_init(void)
+{
+}
 
 void __init ev96100_setup(void)
 {
-	unsigned long config = read_32bit_cp0_register(CP0_CONFIG);
-	unsigned long status = read_32bit_cp0_register(CP0_STATUS);
-	unsigned long info = read_32bit_cp0_register(CP0_INFO);
+	unsigned int config = read_c0_config();
+	unsigned int status = read_c0_status();
+	unsigned int info = read_c0_info();
 	u32 tmp;
 
 	char *argptr;
 
-	clear_cp0_status(ST0_FR);
+	clear_c0_status(ST0_FR);
 
         if (config & 0x8) {
             printk("Secondary cache is enabled\n");
