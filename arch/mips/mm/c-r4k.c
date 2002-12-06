f2a84cb75f542941c210c945615cbcdd0ee31522
Ralf Baechle <ralf@linux-mips.org>
2002-12-06 18:46:47 +0000

    Fix bcops problem.

--- arch/mips/mm/c-r4k.c
+++ arch/mips/mm/c-r4k.c
@@ -1356,7 +1356,7 @@ static inline void __init setup_scache(unsigned int config)
 	case CPU_R5000:
 	case CPU_NEVADA:
 			setup_noscache_funcs();
-#if defined(CONFIG_CPU_R5000) || defined(CONFIG_CPU_NEVADA)
+#ifdef CONFIG_R5000_CPU_SCACHE
 			r5k_sc_init();
 #endif
 			break;
