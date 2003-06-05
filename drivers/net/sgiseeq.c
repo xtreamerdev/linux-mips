a8c0c984048c2f31625746a49fcf6a006119329f
Ralf Baechle <ralf@linux-mips.org>
2003-06-05 09:14:11 +0000

    Don't use NETIF_F_DYNALLOC.

--- drivers/net/sgiseeq.c
+++ drivers/net/sgiseeq.c
@@ -621,8 +621,6 @@ int sgiseeq_init(struct hpc3_regs* regs, int irq)
 		free_page((unsigned long) sp);
 		return -ENOMEM;
 	}
-	/* let unregister_netdev free memory for us */
-	dev->features |= NETIF_F_DYNALLOC;
 
 	if (request_irq(irq, sgiseeq_interrupt, 0, sgiseeqstr, dev)) {
 		printk(KERN_ERR "Seeq8003: Can't get irq %d\n", dev->irq);
@@ -711,6 +709,7 @@ static void __exit sgiseeq_exit(void)
 		free_irq(dev->irq, dev);
 		free_page((unsigned long) sp);
 		unregister_netdev(dev);
+		kfree(dev);
 		dev = next;
 	}
 }
