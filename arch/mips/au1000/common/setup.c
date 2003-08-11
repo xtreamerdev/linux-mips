70b599cdfdfdc13c29f33e87de769c2ea3a6892b
Pete Popov <ppopov@embeddedalley.com>
2003-08-11 05:18:09 +0000

    Small define buglet.

--- arch/mips/au1000/common/setup.c
+++ arch/mips/au1000/common/setup.c
@@ -170,7 +170,7 @@ void __init au1x00_setup(void)
 
 #ifndef CONFIG_SERIAL_NONSTANDARD
 	/* don't touch the default serial console */
-        au_writel(0, UART_ADDR + UART_CLK);
+        au_writel(0, UART0_ADDR + UART_CLK);
 #endif
 
 #ifdef CONFIG_BLK_DEV_IDE
