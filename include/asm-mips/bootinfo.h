3260e0d5a1ed89bece3163bdde1d4ed622e962f9
Pete Popov <ppopov@embeddedalley.com>
2003-09-08 00:05:31 +0000

    Cogent CSB250 board support, by Embedded Edge.

--- include/asm-mips/bootinfo.h
+++ include/asm-mips/bootinfo.h
@@ -176,6 +176,7 @@
 #define MACH_DB1500		5       /* Au1500-based eval board */
 #define MACH_XXS1500		6       /* Au1500-based eval board */
 #define MACH_MTX1		7	/* 4G MTX-1 Au1500-based board */
+#define MACH_CSB250		8	/* Cogent Au1500 */
 
 /*
  * Valid machtype for group NEC_VR41XX
