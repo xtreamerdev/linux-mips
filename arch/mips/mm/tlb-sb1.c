a015a7a43f9316e63ba2e1d958c6151f4cd0a510
Ralf Baechle <ralf@linux-mips.org>
2003-10-13 16:24:19 +0000

    Uh...  set_pagemask got renamed in the meantime ...

--- arch/mips/mm/tlb-sb1.c
+++ arch/mips/mm/tlb-sb1.c
@@ -291,7 +291,7 @@ void __init add_wired_entry(unsigned long entrylo0, unsigned long entrylo1,
  */
 void sb1_tlb_init(void)
 {
-	set_pagemask(PM_DEFAULT_MASK);
+	write_c0_pagemask(PM_DEFAULT_MASK);
 	write_c0_wired(0);
 
 	/*
