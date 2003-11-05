8413b3100f1edfc869be4b0f28474acccf57cb82
Ralf Baechle <ralf@linux-mips.org>
2003-11-05 20:53:00 +0000

    Remove dead IP32 bits.

--- arch/mips/arc/identify.c
+++ arch/mips/arc/identify.c
@@ -44,11 +44,6 @@ static struct smatch mach_table[] = {
 		MACH_GROUP_SGI,
 		MACH_SGI_IP28,
 		PROM_FLAG_ARCS
-	}, {	"SGI-IP32",
-		"SGI IP32",
-		MACH_GROUP_SGI,
-		MACH_SGI_IP32,
-		PROM_FLAG_ARCS
 	}, {	"Microsoft-Jazz",
 		"Jazz MIPS_Magnum_4000",
 		MACH_GROUP_JAZZ,
