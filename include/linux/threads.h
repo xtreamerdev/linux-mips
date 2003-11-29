766134447e91429440c4cb89d07fc4db18389ba1
Ralf Baechle <ralf@linux-mips.org>
2003-11-29 03:42:17 +0000

    Cleanup whitespace garbage diff.

--- include/linux/threads.h
+++ include/linux/threads.h
@@ -9,9 +9,9 @@
  */
  
 #ifdef CONFIG_SMP
-#define NR_CPUS		CONFIG_NR_CPUS
+#define NR_CPUS	CONFIG_NR_CPUS
 #else
-#define NR_CPUS		1
+#define NR_CPUS	1
 #endif
 
 #define MIN_THREADS_LEFT_FOR_ROOT 4
