30606636a588eec9b7337c2a018b80ebc7470baa
Ralf Baechle <ralf@linux-mips.org>
2004-01-20 14:58:03 +0000

    Missing 24K and 25KF PrId values.

--- include/asm-mips/cpu.h
+++ include/asm-mips/cpu.h
@@ -65,7 +65,8 @@
 #define PRID_IMP_20KC		0x8200
 #define PRID_IMP_4KEC		0x8400
 #define PRID_IMP_4KSC		0x8600
-
+#define PRID_IMP_25KF		0x8800
+#define PRID_IMP_24K		0x9300
 
 #define PRID_IMP_UNKNOWN	0xff00
 
