5d707120ea3cdd4b3d93472e8cd7c7aab4c497a1
Pete Popov <ppopov@embeddedalley.com>
2004-01-30 07:25:37 +0000

    - remove USB_DEVICE setup. Pb1550 does it differently - set default memory to 128MB - fix the system type returned from get_system_type()

--- arch/mips/au1000/pb1550/board_setup.c
+++ arch/mips/au1000/pb1550/board_setup.c
@@ -55,13 +55,6 @@ void __init board_setup(void)
 	u32 pin_func;
 	rtc_ops = &no_rtc_ops;
 
-#ifdef CONFIG_AU1X00_USB_DEVICE
-	// 2nd USB port is USB device
-	pin_func = au_readl(SYS_PINFUNC) & (u32)(~0x8000);
-	au_writel(pin_func, SYS_PINFUNC);
-#endif
-
 	au_writel(0, (u32)bcsr|0x10); /* turn off pcmcia power */
-
-    printk("AMD Alchemy Pb1550 Board\n");
+	printk("AMD Alchemy Pb1550 Board\n");
 }
