77beeaab7554988579ec6116249589b37d7a0511
Ladislav Michl <ladis@linux-mips.org>
2004-09-23 06:26:22 +0000

    fix typecast

--- arch/mips/sgi-ip22/ip22-setup.c
+++ arch/mips/sgi-ip22/ip22-setup.c
@@ -141,7 +141,8 @@ void __init ip22_setup(void)
 
 	/* Set EISA IO port base for Indigo2
 	 * ioremap cannot fail */
-	set_io_port_base((phys_t)ioremap(0x00080000, 0xffffffff - 0x00080000));
+	set_io_port_base((unsigned long)ioremap(0x00080000,
+						0xffffffff - 0x00080000));
 
 	/* Nothing registered console before us, so simply use first entry */
 	c = &console_cmdline[0];
