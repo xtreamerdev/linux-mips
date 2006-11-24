65f620c7652596301270da0c00570c61a3ab3c5a
Ralf Baechle <ralf@linux-mips.org>
2006-11-24 12:17:51 +0000

    [MIPS] Fix Bonito bootup message.
    Even when enabling Bonito IOBC coherence the kernel would actually claim
    it was disabling it.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from fbb4626b88976dc481fd1ca8ee5b77e8391c53b3 commit)

--- arch/mips/mips-boards/malta/malta_setup.c
+++ arch/mips/mips-boards/malta/malta_setup.c
@@ -161,7 +161,7 @@ void __init plat_setup(void)
 				BONITO_PCIMEMBASECFG |=
 					(BONITO_PCIMEMBASECFG_MEMBASE0_CACHED |
 					 BONITO_PCIMEMBASECFG_MEMBASE1_CACHED);
-				printk("Disabled Bonito IOBC coherency\n");
+				printk("Enabled Bonito IOBC coherency\n");
 			}
 		}
 		else
