4aa6ccf9b13251c17aef1e5fb9f3b14e6d898f9a
Ralf Baechle <ralf@linux-mips.org>
2007-01-20 00:18:01 +0000

    [MIPS] SMTC: Instant IPI replay.
    SMTC pseudo-interrupts between TCs are deferred and queued if the target
    TC is interrupt-inhibited (IXMT). In the first SMTC prototypes, these
    queued IPIs were serviced on return to user mode, or on entry into the
    kernel idle loop. The INSTANT_REPLAY option dispatches them as part of
    local_irq_restore() processing, which adds runtime overhead (hence the
    option to turn it off), but ensures that IPIs are handled promptly even
    under heavy I/O interrupt load.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 77274fa2370e11dafa0ba6a1ef3eb5b2ed0a6665)

--- include/asm-mips/interrupt.h
+++ include/asm-mips/interrupt.h
@@ -14,6 +14,27 @@
 #include <linux/config.h>
 #include <asm/hazards.h>
 
+/*
+ * MIPS_MT_SMTC_INSTANT_REPLAY does prompt replay of deferred IPIs,
+ * at the cost of branch and call overhead on each local_irq_restore()
+ */
+
+#ifdef CONFIG_MIPS_MT_SMTC_INSTANT_REPLAY
+
+extern void smtc_ipi_replay(void);
+
+#define irq_restore_epilog(flags)					\
+do {									\
+	if (!(flags & 0x0400))						\
+		smtc_ipi_replay();					\
+} while (0)
+
+#else
+
+#define irq_restore_epilog(ignore) do { } while (0)
+
+#endif /* CONFIG_MIPS_MT_SMTC_INSTANT_REPLAY */
+
 __asm__ (
 	"	.macro	local_irq_enable				\n"
 	"	.set	push						\n"
@@ -209,6 +230,7 @@ do {									\
 		: "=r" (__tmp1)						\
 		: "0" (flags)						\
 		: "memory");						\
+	irq_restore_epilog(flags);					\
 } while(0)
 
 static inline int irqs_disabled(void)
