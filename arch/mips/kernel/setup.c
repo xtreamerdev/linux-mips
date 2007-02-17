a72bff8edd9c34830d2fc4cc819772166bc276d3
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2007-02-18 01:02:14 +0900

    [MIPS] Make some __setup functions static
    This fixes some sparse warnings. ("warning: symbol 'foo' was not
    declared. Should it be static?")
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 5333ccc3c2f25aade3653f056cec9eba12209302)

--- arch/mips/kernel/setup.c
+++ arch/mips/kernel/setup.c
@@ -545,7 +545,7 @@ void __init setup_arch(char **cmdline_p)
 #endif
 }
 
-int __init fpu_disable(char *s)
+static int __init fpu_disable(char *s)
 {
 	int i;
 
@@ -557,7 +557,7 @@ int __init fpu_disable(char *s)
 
 __setup("nofpu", fpu_disable);
 
-int __init dsp_disable(char *s)
+static int __init dsp_disable(char *s)
 {
 	cpu_data[0].ases &= ~MIPS_ASE_DSP;
 
