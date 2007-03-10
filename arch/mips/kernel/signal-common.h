73dfac448c5bf179792b01b2fdd6012fe937cef1
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2007-03-10 23:54:14 +0900

    [MIPS] Cleanup FPU ownership management
    Currently preempt_disable/preempt_enable are scattered in FPU
    ownership management code.  This patch makes own_fpu() and lose_fpu()
    can save/restore FPU context in itself and make these functions (and
    init_fpu() too) preempt-proof.  This makes the FPU management codes
    much readable.  Also this patch introduce raw_cpu_has_fpu macro which
    is to be used if the caller did not need atomic context.
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit fc1ac60f7532ff5472fa8720a12ff4ccaa079ee0)

--- arch/mips/kernel/signal-common.h
+++ arch/mips/kernel/signal-common.h
@@ -51,14 +51,7 @@ setup_sigcontext(struct pt_regs *regs, struct sigcontext __user *sc)
 	 * Save FPU state to signal context.  Signal handler will "inherit"
 	 * current FPU state.
 	 */
-	preempt_disable();
-
-	if (!is_fpu_owner()) {
-		own_fpu();
-		restore_fp(current);
-	}
-
-	preempt_enable();
+	own_fpu(1);
 	enable_fp_in_kernel();
 	err |= save_fp_context(sc);
 	disable_fp_in_kernel();
@@ -124,20 +117,16 @@ restore_sigcontext(struct pt_regs *regs, struct sigcontext __user *sc)
 	err |= __get_user(used_math, &sc->sc_used_math);
 	conditional_used_math(used_math);
 
-	preempt_disable();
-
 	if (used_math()) {
 		/* restore fpu context if we have used it before */
-		own_fpu();
-		preempt_enable();
+		own_fpu(0);
 		enable_fp_in_kernel();
 		if (!err)
 			err = check_and_restore_fp_context(sc);
 		disable_fp_in_kernel();
 	} else {
 		/* signal handler may have used FPU.  Give it up. */
-		lose_fpu();
-		preempt_enable();
+		lose_fpu(0);
 	}
 
 	return err;
