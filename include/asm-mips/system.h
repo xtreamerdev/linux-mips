9d6fdad6eab4deca1b974b76c6a85c03ee03e6d3
Ralf Baechle <ralf@linux-mips.org>
2007-07-09 14:41:34 +0100

    [MIPS] Enable support for the userlocal hardware register
    Which will cut down the cost of RDHWR $29 which is used to obtain the
    TLS pointer and so far being emulated in software down to a single cycle
    operation.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 401e3580f1fd4e90d0cbc24a3af34465d08de152)

--- include/asm-mips/system.h
+++ include/asm-mips/system.h
@@ -58,6 +58,8 @@ do {									\
 	(last) = resume(prev, next, task_thread_info(next));		\
 	if (cpu_has_dsp)						\
 		__restore_dsp(current);					\
+	if (cpu_has_userlocal)						\
+		write_c0_userlocal(task_thread_info(current)->tp_value);\
 } while(0)
 
 #else
@@ -68,6 +70,8 @@ do {									\
 	(last) = resume(prev, next, task_thread_info(next));		\
 	if (cpu_has_dsp)						\
 		__restore_dsp(current);					\
+	if (cpu_has_userlocal)						\
+		write_c0_userlocal(task_thread_info(current)->tp_value);\
 } while(0)
 #endif
 
