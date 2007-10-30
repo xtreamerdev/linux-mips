97ad02332100ea0b8786682282d3ac9b508ea563
Ralf Baechle <ralf@linux-mips.org>
2007-10-30 17:25:26 +0000

    [MIPS] Fix context DSP context / TLS pointer switching bug for new threads.
    A new born thread starts execution not in schedule but rather in
    ret_from_fork which results in it bypassing the part of the code to
    load a new context written in C which are the DSP context and the
    userlocal register which Linux uses for the TLS pointer.  Frequently
    we were just getting away with this bug for a number of reasons:
    
     o Real world application scenarios are very unlikely to use clone or fork
       in blocks of DSP code.
     o Linux by default runs the child process right after the fork, so the
       child by luck will find all the right context in the DSP and userlocal
       registers.
     o So far the rdhwr instruction was emulated on all hardware so userlocal
       wasn't getting referenced at all and the emulation wasn't suffering
       from the issue since it gets it's value straight from the thread's
       thread_info.
    
    Fixed by moving the code to load the context from switch_to() to
    finish_arch_switch which will be called by newborn and old threads.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit d74533e74d7ebc2707512f3736f98755ad66b1f0)

--- include/asm-mips/system.h
+++ include/asm-mips/system.h
@@ -180,11 +180,7 @@ do {									\
 		__save_dsp(prev);					\
 	next->thread.emulated_fp = 0;					\
 	(last) = resume(prev, next, next->thread_info);			\
-	if (cpu_has_dsp)						\
-		__restore_dsp(current);					\
-	if (cpu_has_userlocal)						\
-		write_c0_userlocal(task_thread_info(current)->tp_value);\
-} while(0)
+} while (0)
 
 #else
 #define switch_to(prev,next,last)					\
@@ -192,12 +188,16 @@ do {									\
 	if (cpu_has_dsp)						\
 		__save_dsp(prev);					\
 	(last) = resume(prev, next, task_thread_info(next));		\
+} while (0)
+#endif
+
+#define finish_arch_switch(prev)					\
+do {									\
 	if (cpu_has_dsp)						\
 		__restore_dsp(current);					\
 	if (cpu_has_userlocal)						\
-		write_c0_userlocal(task_thread_info(current)->tp_value);\
-} while(0)
-#endif
+		write_c0_userlocal(current_thread_info()->tp_value);	\
+} while (0)
 
 /*
  * On SMP systems, when the scheduler does migration-cost autodetection,
