888caf3ccca175955985511b03f17852c63a13c7
Thomas Bogendoerfer <tsbogend@alpha.franken.de>
2008-03-26 16:42:54 +0100

    [MIPS] Add missing 4KEC TLB refill handler
    Early 4KEc were MIPS32r1 and therefore need some love to get a TLB
    refill handler.
    
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit a82f2241b35c6964d67a5ee5480ef6be8d99339a)

--- arch/mips/mm/tlbex.c
+++ arch/mips/mm/tlbex.c
@@ -903,6 +903,7 @@ static __cpuinit void build_tlb_write_entry(u32 **p, struct label **l,
 	case CPU_R12000:
 	case CPU_R14000:
 	case CPU_4KC:
+	case CPU_4KEC:
 	case CPU_SB1:
 	case CPU_SB1A:
 	case CPU_4KSC:
